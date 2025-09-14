6eeb38abc64d904fb0720cb94104689b9cc4f589
Marija <marija@fuel.me>
2025-09-14 11:49:40 +0200

    feat: Added more interfaces based on API response

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,12 +1,35 @@
+interface Geo {
+    lat: string
+    lon: string
+}
+interface Address {
+    street: string
+    suite: string
+    city: string
+    zipcode: string
+    geo: Geo
+}
+
+interface Company {
+    name: string
+    cathPhrase: string
+    bs: string
+}
+
 export interface User {
     readonly id: number
     name: string
     username: string
     email: string
+    address: Address
+    phone: string
+    website: string
+    company: Company
 }
 
 export interface Post {
     readonly id: number
+    userId: number
     title: string
     body: string
 }
